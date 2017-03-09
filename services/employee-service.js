5903c11c620cc80e360c30865bc89a1c73a0384d
Rich Kelly <rkelly@iqn.com>
2017-03-09 14:04:37 -0700

    Added support to save skills under employee and into roles/skills if new.

--- services/employee-service.js
+++ services/employee-service.js
@@ -15,35 +15,77 @@ EmployeeService.prototype.getUserName = function(userId, bot, callback) {
 	});
 };
 
-EmployeeService.prototype.addEmployeeAndRole = function (employeeName, role, firebase, callback)
+EmployeeService.prototype.addEmployeeAndRoleAndSkills = function (employeeName, role, skills, firebase, done)
 {
 	logger.info("Adding employee " + employeeName + " with role " + role);
-	addEmployee(employeeName, role, firebase, function ()
-	{
-		callback(null);
+	waterfall([
+	    //Add role for employee
+		function (callback)
+		{
+			addEmployeeRole(employeeName, role, firebase, callback);
+		},
+
+		//Get employees's current skills so that we do not add duplicates
+		function (callback) {
+			getEmployeeSkills(employeeName, firebase, callback);
+		},
+
+		//Add new skills for employee
+		function (currentSkills, callback)
+		{
+			var newSkills = []
+			for (var i = 0; i < skills.length; i++) {
+				var found = false;
+				for (var j = 0; j < currentSkills.length; j++) {
+					if (skills[i] == currentSkills[j]) {
+						found = true;
+					}
+				}
+				if (!found) {
+					newSkills.push(skills[i]);
+				}
+			}
+			if (newSkills.length > 0) {
+				addEmployeeSkills(employeeName, newSkills, firebase, callback);
+			}
+		}
+	], function(err, data1, data2) {
+		if (err)
+		{
+			logger.error("Fatal error during addEmployeeAndRoleAndSkills: " + err);
+		}
+		done(null);
 	});
 };
 
-//EmployeeService.prototype.getEmployee = function(employeeName, firebase, callback) {
-//	var dbRef = firebase.database().ref("employees/"+ employeeName);
-//	dbRef.once("value", function(snapshot) {
-//		callback(null, snapshot.val());
-//	});
-//};
-
-//var getEmployee = function(employeeName, firebase, callback) {
-//	var dbRef = firebase.database().ref("employees/"+ employeeName);
-//	dbRef.once("value", function(snapshot) {
-//		console.log("Snapshot of employee " + snapshot.val());
-//		callback(null, snapshot.val());
-//	});
-//};
-
-var addEmployee = function(employeeName, role, firebase, callback) {
+var addEmployeeRole = function(employeeName, role, firebase, callback) {
 	var dbRef = firebase.database().ref('employees/' + employeeName + '/roles');
 	dbRef.child(role).set('true');
+	logger.info("Added employee " + employeeName + " with role " + role);
 	callback(null);
 };
 
+var addEmployeeSkills = function(employeeName, skills, firebase, callback) {
+	var dbRef = firebase.database().ref('employees/' + employeeName);
+	for (var i = 0; i < skills.length; i++)
+	{
+		dbRef.child('skills').push(skills[i]);
+		logger.info("Added employee " + employeeName + " with skill " + skills[i]);
+	}
+	callback(null);
+};
+
+function getEmployeeSkills(employeeName, firebase, callback) {
+	var skills = []
+	var dbRef = firebase.database().ref('employees/' + employeeName + '/skills');
+	dbRef.once("value", function(snapshot) {
+		if (snapshot.val()) {
+			snapshot.forEach(function(childSnapshot) {
+				skills.push(childSnapshot.val())
+			})
+		}
+		callback(null, skills);
+	});
+}
 
 module.exports = new EmployeeService();
--- services/interview-service.js
+++ services/interview-service.js
@@ -14,13 +14,14 @@ function InterviewService() {
 
 InterviewService.prototype.handle = function(firebase, bot, message) {
 	var role;
+	var roleSkills;
 	var userName;
-	var isNewRole;
 	waterfall([
 			//Get user name from user id stored in message
 			function (callback) {
 				EmployeeService.getUserName(message.user, bot, callback)
 			},
+
 			//Get all Roles in db
 			function (username, callback)
 			{
@@ -29,52 +30,87 @@ InterviewService.prototype.handle = function(firebase, bot, message) {
 					callback(null, roles);
 				});
 			},
+
 			//Ask user for their role. Let them add new role if not listed.
 			function ( roles, callback) {
 				getUsersRole(roles, firebase, bot, message, function(err, role, isNewRole) {
 					callback(null, role, isNewRole);
 				});
 			},
-			//if User indicated a role, then add user to employee and add their role
-			function (role, isNewRole, callback) {
+
+			//if User indicated an existing role, then let's get the skills associated to that role.
+			//If no role specified, break from waterfall. If new role then skip getting skills since n/a.
+			function (roleSelected, isNewRole, callback) {
+				role = roleSelected;
 				if (role != null)
 				{
-					logger.info("User " + userName + " chose role " + role + " which is new role:" + isNewRole);
-					EmployeeService.addEmployeeAndRole(userName, role, firebase, function (err)
-					{
-						callback(null, role, isNewRole);
-					});
+					if (!isNewRole) {
+						RoleService.getSkills(role, firebase, function(err, skills)	{
+							callback(null, skills);
+						});
+					} else {
+						callback(null, []); //no skills
+					}
+				} else {
+					callback('break')
 				}
 			},
-			//Tell user their role was added
-		    function (role, isNewRole, callback) {
-				bot.reply(message, "Added employee " + userName + " with role " + role);
-				if (isNewRole) {
-					RoleService.addRole(role, firebase);
-				}
-			} //,
-			//function(role, isNewRole, callback) {
-			//	if (isNewRole) {
-			//		RoleService.addRole(role);
-			//	}
-			//}
 
-	],  function (err) { // the "complete" callback of `async.waterfall`
-		if ( err ) {
-			logger.error("Fatal error during getting user role: " + err);
+			//Let's ask user what skills they have. Let them add new ones not in db.
+			function (skills, callback) {
+				roleSkills = skills;
+				getUsersSkills(roleSkills, firebase, bot, message, function(err, userSkills) {
+					callback(null, userSkills);
+				});
+			},
+
+			//if User indicated skills, then add user to employee and add their role and skills
+			function (userSkills, callback) {
+				if (userSkills != null && userSkills.length > 0)
+				{
+					EmployeeService.addEmployeeAndRoleAndSkills(userName, role, userSkills, firebase, function (err)
+					{
+						callback(null, userSkills);
+					});
+				} else {  //no skills specified, save nothing
+					callback('break')
+				}
+			},
+
+			//Tell user their role was added and add role and skills (if new) to roles db
+		    function (userSkills, callback) {
+				bot.reply(message, "Added you to employee db with user name " + userName + " and role " + role + " and skills specified.");
+
+				//Now add new role and skills to roles db
+				for (var i = 0; i < userSkills.length; i++) {
+					var found = false;
+					for (var j = 0; j < roleSkills.length; j++) {
+						if (userSkills[i] == roleSkills[j]) {
+							found = true;
+						}
+					}
+					if (!found) {
+						RoleService.addRoleAndSkill(role, userSkills[i], firebase);
+					}
+				}
+			}
+
+	], function (err)
+	{
+		if (err) {
+			if (err != 'break')	{
+				logger.error("Fatal error during getting user role: " + err);
+			}
+			else {
+				err = null;
+			}
 		}
 		callback(err);
 	});
 }
 
 var getUsersRole = function (roles, firebase, bot, message, callback) {
-	var role;
-	var askMsg = "Choose the number of your primary role:" + os.EOL;
-	for(var i=0; i< roles.length; i++) {
-		askMsg += i+1 + ") " + roles[i] + os.EOL;
-	}
-	askMsg += " OR enter a new role if you don't see yours." + os.EOL;
-	askMsg += " OR enter 'quit' if you want to quit the interview without saving."
+	var askMsg = buildAskRoleMsg(roles);
 	bot.startConversation(message, function(err, convo) {
 		convo.ask(askMsg, [
 			{
@@ -82,8 +118,7 @@ var getUsersRole = function (roles, firebase, bot, message, callback) {
 				callback: function(response, convo) {
 					var n = response.text
 					if (n <= roles.length) {
-						role =  roles[n-1];
-						callback(null, role, false );
+						callback(null, roles[n-1], false );
 						convo.stop();
 					} else {
 						convo.say("Invalid choice " + n + ". Try again or enter 'quit'");
@@ -92,31 +127,93 @@ var getUsersRole = function (roles, firebase, bot, message, callback) {
 					}
 				}
 			},
-			//{
-			//	pattern: 'new',
-			//	callback: function(response, convo) {
-			//		convo.stop();
-			//		RoleService.teachNewRole(firebase, bot, message);
-			//	}
-			//},
 			{
 				pattern: '^quit',
 				callback: function(response, convo) {
-					callback(null, role, false );
+					callback(null, null, false );
 					convo.stop();
 				}
 			},
 			{
 				default: true,
 				callback: function(response, convo) {
-					//User entered new role, so use it. TODO would be to verify w/user if they really meant to
-					role =  response.text.toLowerCase();
-					callback(null, role, true); //Indicate new role so we add it to Roles
+					//User entered new role, so use it.
+					callback(null, response.text.toLowerCase(), true); //Indicate new role
 					convo.stop();
 				}
 			}
 		]);
 	});
+}
+
+var getUsersSkills = function (roleSkills, firebase, bot, message, callback) {
+	var userSkills = [];
+	var askSkillsMsg = buildAskSkillsMsg(roleSkills);
+	bot.startConversation(message, function (err, convo)
+	{
+		convo.ask(askSkillsMsg, [
+			{
+				pattern: '^[1-9][0-9]*$',
+				callback: function (response, convo)
+				{
+					var n = response.text
+					if (n <= roleSkills.length) {
+						userSkills.push(roleSkills[n - 1]);
+					} else {
+						convo.say("Invalid choice " + n + ". Try again or enter 'quit'");
+					}
+					convo.repeat();
+					convo.next();
+				}
+			},
+			{
+				pattern: '^quit',
+				callback: function (response, convo)
+				{
+					callback(null, userSkills);
+					convo.stop();
+				}
+			},
+			{
+				default: true,
+				callback: function (response, convo)
+				{
+					//User entered new role, so add to user's skill set
+					userSkills.push(response.text);
+					convo.repeat();
+					convo.next();
+				}
+			}
+		]);
+	});
+}
+
+function buildAskRoleMsg(roles)
+{
+	var askMsg = "Choose the number of your primary role:" + os.EOL;
+	for(var i=0; i< roles.length; i++) {
+		askMsg += i+1 + ") " + roles[i] + os.EOL;
+	}
+	askMsg += " OR enter a new role if you don't see yours." + os.EOL;
+	askMsg += " OR enter 'quit' if you want to quit the interview without saving."
+	return askMsg;
+}
+
+function buildAskSkillsMsg(skills) {
+	var askMsg;
+	if (skills != null && skills.length > 0)
+	{
+		askMsg = "Choose the number of your skill:" + os.EOL;
+		for (var i = 0; i < skills.length; i++)
+		{
+			askMsg += i + 1 + ") " + skills[i] + os.EOL;
+		}
+		askMsg += " OR enter a new skill if you don't see yours." + os.EOL;
+	} else {
+		askMsg = "Enter a skill associated with your role." + os.EOL;
+	}
+	askMsg += " OR enter 'quit' if you are done entering your skills set."
+	return askMsg;
 }
 
 module.exports = new InterviewService();
